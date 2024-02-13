8b028ee9e3a7c262eee32b0a2cc4f8f90cc84a5f
LidorLevi123 <lidorlevi728@gmail.com>
2024-02-13 20:56:07 +0200

    Connect backend to cloud db

--- backend/config/dev.js
+++ backend/config/dev.js
@@ -1,4 +1,5 @@
 export default  {
-  dbURL: 'mongodb://127.0.0.1:27017',
+  // dbURL: 'mongodb://127.0.0.1:27017',
+  dbURL: 'mongodb+srv://lidorle123:dwd137@cluster0.frwxxud.mongodb.net/',
   dbName : 'SwitchApp'
 }
