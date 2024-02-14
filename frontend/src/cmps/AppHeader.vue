cef708436ff6f20e1107c6f90c4feffa0ffc4b0f
LidorLevi123 <lidorlevi728@gmail.com>
2024-02-14 23:19:22 +0200

    Add menu icon for mobile

--- frontend/src/cmps/AppHeader.vue
+++ frontend/src/cmps/AppHeader.vue
@@ -2,39 +2,62 @@
     <section class="app-header main-layout full">
         <div class="app-header-container flex justify-between align-center">
             <div class="logo">SwitchApp</div>
-            <nav class="main-nav-container">
+            <Backdrop :class="menuClass" @click="onToggleMenu"/>
+            <nav class="main-nav-container" :class="menuClass">
                 <ul class="main-nav flex">
-                    <li><RouterLink to="/">Resources</RouterLink></li>
-                    <li><RouterLink to="/switch/search">Search</RouterLink></li>
-                    <li><RouterLink to="/login" 
-                        @click="onLogout"
-                        :title="title">{{ msg() }}</RouterLink></li>
+                    <li>
+                        <RouterLink to="/">Resources</RouterLink>
+                    </li>
+                    <li>
+                        <RouterLink to="/switch/search">Search</RouterLink>
+                    </li>
+                    <li>
+                        <RouterLink to="/login" @click="onLogout" :title="title">{{ msg() }}</RouterLink>
+                    </li>
                 </ul>
             </nav>
+            <img class="menu-icon" src="../assets/img/menu.png" alt="menu icon" @click="onToggleMenu">
         </div>
     </section>
 </template>
 
 <script>
+import Backdrop from './Backdrop.vue'
 export default {
     name: 'AppHeader',
 
     props: { user: Object },
 
+    data() {
+        return {
+            isMenuOpen: false
+        }
+    },
+
     methods: {
         onLogout() {
-            if(!this.user) return
+            if (!this.user) return
             this.$emit('logout')
         },
         msg() {
             return this.user ? `Logout` : 'Login'
+        },
+        onToggleMenu() {
+            this.isMenuOpen = !this.isMenuOpen
         }
     },
 
     computed: {
         title() {
-            if(this.user) return 'Logout'
+            if (this.user) return 'Logout'
+        },
+        menuClass() {
+            return this.isMenuOpen ? 'menu-open' : ''
         }
+    },
+
+    components: {
+        Backdrop
     }
 }
 </script>
---
+++ frontend/src/cmps/Backdrop.vue
@@ -0,0 +1,9 @@
+<template>
+    <section class="backdrop"></section>
+</template>
+
+<script>
+export default {
+    name: 'Backdrop',
+}
+</script>
